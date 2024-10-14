c7ed8e2279113c231df1a90f2cb81daae8b85174
KOSASIH <kosasihg88@gmail.com>
2024-10-14 09:51:38 +0700

    Update life_support.py

--- src/habitat/modules/life_support/life_support.py
+++ src/habitat/modules/life_support/life_support.py
@@ -2,204 +2,223 @@
 
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
 from scipy.optimize import minimize
 from scipy.integrate import odeint
 from matplotlib.pyplot import plot, show, xlabel, ylabel, title
+import rospy
+import roslib
+import tf
 
 class LifeSupportSystem:
     def __init__(self, habitat):
         self.habitat = habitat
-        self.air_quality_control = AirQualityControl()
-        self.water_purification = WaterPurification()
+        self.air_supply = AirSupply()
+        self.water_supply = WaterSupply()
         self.waste_management = WasteManagement()
         self.temperature_control = TemperatureControl()
         self.humidity_control = HumidityControl()
+        self.lighting_control = LightingControl()
+        self.air_quality_control = AirQualityControl()
 
-    def monitor_air_quality(self):
-        air_quality_data = self.air_quality_control.get_air_quality_data()
-        air_quality_index = self.air_quality_control.calculate_air_quality_index(air_quality_data)
-        return air_quality_index
+    def monitor_life_support(self):
+        air_quality = self.air_supply.get_air_quality()
+        water_quality = self.water_supply.get_water_quality()
+        waste_level = self.waste_management.get_waste_level()
+        temperature = self.temperature_control.get_temperature()
+        humidity = self.humidity_control.get_humidity()
+        lighting_level = self.lighting_control.get_lighting_level()
+        air_quality_level = self.air_quality_control.get_air_quality_level()
+        return air_quality, water_quality, waste_level, temperature, humidity, lighting_level, air_quality_level
 
-    def control_air_quality(self, air_quality_index):
-        if air_quality_index < 50:
-            self.air_quality_control.activate_air_purifiers()
-        elif air_quality_index > 80:
-            self.air_quality_control.deactivate_air_purifiers()
+    def control_life_support(self, air_quality, water_quality, waste_level, temperature, humidity, lighting_level, air_quality_level):
+        if air_quality < self.habitat.air_quality_requirement:
+            self.air_supply.activate_air_supply()
+        elif air_quality > self.habitat.air_quality_requirement:
+            self.air_supply.deactivate_air_supply()
 
-    def monitor_water_quality(self):
-        water_quality_data = self.water_purification.get_water_quality_data()
-        water_quality_index = self.water_purification.calculate_water_quality_index(water_quality_data)
-        return water_quality_index
+        if water_quality < self.habitat.water_quality_requirement:
+            self.water_supply.activate_water_supply()
+        elif water_quality > self.habitat.water_quality_requirement:
+            self.water_supply.deactivate_water_supply()
 
-    def control_water_quality(self, water_quality_index):
-        if water_quality_index < 50:
-            self.water_purification.activate_water_purifiers()
-        elif water_quality_index > 80:
-            self.water_purification.deactivate_water_purifiers()
+        if waste_level > self.habitat.waste_level_requirement:
+            self.waste_management.activate_waste_management()
+        elif waste_level < self.habitat.waste_level_requirement:
+            self.waste_management.deactivate_waste_management()
 
-    def monitor_waste_management(self):
-        waste_management_data = self.waste_management.get_waste_management_data()
-        waste_management_index = self.waste_management.calculate_waste_management_index(waste_management_data)
-        return waste_management_index
+        if temperature < self.habitat.temperature_requirement:
+            self.temperature_control.activate_temperature_control()
+        elif temperature > self.habitat.temperature_requirement:
+            self.temperature_control.deactivate_temperature_control()
 
-    def control_waste_management(self, waste_management_index):
-        if waste_management_index < 50:
-            self.waste_management.activate_waste_recycling()
-        elif waste_management_index > 80:
-            self.waste_management.deactivate_waste_recycling()
+        if humidity < self.habitat.humidity_requirement:
+            self.humidity_control.activate_humidity_control()
+        elif humidity > self.habitat.humidity_requirement:
+            self.humidity_control.deactivate_humidity_control()
 
-    def monitor_temperature(self):
-        temperature_data = self.temperature_control.get_temperature_data()
-        temperature_index = self.temperature_control.calculate_temperature_index(temperature_data)
-        return temperature_index
+        if lighting_level < self.habitat.lighting_level_requirement:
+            self.lighting_control.activate_lighting_control()
+        elif lighting_level > self.habitat.lighting_level_requirement:
+            self.lighting_control.deactivate_lighting_control()
 
-    def control_temperature(self, temperature_index):
-        if temperature_index < 20:
-            self.temperature_control.activate_heating_system()
-        elif temperature_index > 25:
-            self.temperature_control.deactivate_heating_system()
+        if air_quality_level < self.habitat.air_quality_level_requirement:
+            self.air_quality_control.activate_air_quality_control()
+        elif air_quality_level > self.habitat.air_quality_level_requirement:
+            self.air_quality_control.deactivate_air_quality_control()
 
-    def monitor_humidity(self):
-        humidity_data = self.humidity_control.get_humidity_data()
-        humidity_index = self.humidity_control.calculate_humidity_index(humidity_data)
-        return humidity_index
+    def optimize_life_support(self):
+        # Use machine learning model to optimize life support
+        X = pd.DataFrame({'air_quality': [self.air_supply.get_air_quality()], 
+                           'water_quality': [self.water_supply.get_water_quality()], 
+                           'waste_level': [self.waste_management.get_waste_level()], 
+                           'temperature': [self.temperature_control.get_temperature()], 
+                           'humidity': [self.humidity_control.get_humidity()], 
+                           'lighting_level': [self.lighting_control.get_lighting_level()], 
+                           'air_quality_level': [self.air_quality_control.get_air_quality_level()]})
+        y = pd.DataFrame({'life_support_performance': [self.monitor_life_support()]})
+        model = RandomForestRegressor(n_estimators=100)
+        model.fit(X, y)
+        optimized_performance = model.predict(X)
+        return optimized_performance
 
-    def control_humidity(self, humidity_index):
-        if humidity_index < 40:
-            self.humidity_control.activate_humidification_system()
-        elif humidity_index > 60:
-            self.humidity_control.deactivate_humidification_system()
-
-class AirQualityControl:
+class AirSupply:
     def __init__(self):
-        self.air_quality_data = pd.DataFrame(columns=['CO2', 'O2', 'NO2', 'SO2'])
-        self.air_purifiers = []
+        self.air_quality = 0
+        self.air_supply_system = []
 
-    def get_air_quality_data(self):
-        return self.air_quality_data
+    def get_air_quality(self):
+        return self.air_quality
 
-    def calculate_air_quality_index(self, air_quality_data):
-        # Use machine learning model to calculate air quality index
-        X = air_quality_data.drop(['air_quality_index'], axis=1)
-        y = air_quality_data['air_quality_index']
-        clf = RandomForestClassifier(n_estimators=100)
-        clf.fit(X, y)
-        air_quality_index = clf.predict(X)
-        return air_quality_index
+    def activate_air_supply(self):
+        # Activate air supply system using ROS
+        rospy.init_node('air_supply_node')
+        self.air_supply_system = ['air_supply_system_1', 'air_supply_system_2', 'air_supply_system_3']
+        self.air_quality = 100
 
-    def activate_air_purifiers(self):
-        # Activate air purifiers using robotics and automation system
-        pass
+    def deactivate_air_supply(self):
+        # Deactivate air supply system using ROS
+        rospy.init_node('air_supply_node')
+        self.air_supply_system = []
+        self.air_quality = 0
 
-    def deactivate_air_purifiers(self):
-        # Deactivate air purifiers using robotics and automation system
-        pass
-
-class WaterPurification:
+class WaterSupply:
     def __init__(self):
-        self.water_quality_data = pd.DataFrame(columns=['pH', 'Turbidity', 'Conductivity'])
-        self.water_purifiers = []
+        self.water_quality = 0
+        self.water_supply_system = []
 
-    def get_water_quality_data(self):
-        return self.water_quality_data
+    def get_water_quality(self):
+        return self.water_quality
 
-    def calculate_water_quality_index(self, water_quality_data):
-        # Use machine learning model to calculate water quality index
-        X = water_quality_data.drop(['water_quality_index'], axis=1)
-        y = water_quality_data['water_quality_index']
-        clf = RandomForestClassifier(n_estimators=100)
-        clf.fit(X, y)
-        water_quality_index = clf.predict(X)
-        return water_quality_index
+    def activate_water_supply(self):
+        # Activate water supply system using ROS
+        rospy.init_node('water_supply_node')
+        self.water_supply_system = ['water_supply_system_1', 'water_supply_system_2', 'water_supply_system_3']
+        self.water_quality = 100
 
-    def activate_water_purifiers(self):
-        # Activate water purifiers using robotics and automation system
-        pass
-
-    def deactivate_water_purifiers(self):
-        # Deactivate water purifiers using robotics and automation system
-        pass
+    def deactivate_water_supply(self):
+        # Deactivate water supply system using ROS
+        rospy.init_node('water_supply_node')
+        self.water_supply_system = []
+        self.water_quality = 0
 
 class WasteManagement:
-    def __init __(self):
-        self.waste_management_data = pd.DataFrame(columns=['Waste_Level', 'Recycling_Rate'])
-        self.waste_recycling_system = []
+    def __init__(self):
+        self.waste_level = 0
+        self.waste_management_system = []
 
-    def get_waste_management_data(self):
-        return self.waste_management_data
+    def get_waste_level(self):
+        return self.waste_level
 
-    def calculate_waste_management_index(self, waste_management_data):
-        # Use machine learning model to calculate waste management index
-        X = waste_management_data.drop(['waste_management_index'], axis=1)
-        y = waste_management_data['waste_management_index']
-        clf = RandomForestClassifier(n_estimators=100)
-        clf.fit(X, y)
-        waste_management_index = clf.predict(X)
-        return waste_management_index
+    def activate_waste_management(self):
+        # Activate waste management system using ROS
+        rospy.init_node('waste_management_node')
+        self.waste_management_system = ['waste_management_system_1', 'waste_management_system_2', 'waste_management_system_3']
+        self.waste_level = 50
 
-    def activate_waste_recycling(self):
-        # Activate waste recycling system using robotics and automation system
-        pass
-
-    def deactivate_waste_recycling(self):
-        # Deactivate waste recycling system using robotics and automation system
-        pass
+    def deactivate_waste_management(self):
+        # Deactivate waste management system using ROS
+        rospy.init_node('waste_management_node')
+        self.waste_management_system = []
+        self.waste_level = 0
 
 class TemperatureControl:
     def __init__(self):
-        self.temperature_data = pd.DataFrame(columns=['Temperature'])
-        self.heating_system = []
+        self.temperature = 0
+        self.temperature_control_system = []
 
-    def get_temperature_data(self):
-        return self.temperature_data
+    def get_temperature(self):
+        return self.temperature
 
-    def calculate_temperature_index(self, temperature_data):
-        # Use machine learning model to calculate temperature index
-        X = temperature_data.drop(['temperature_index'], axis=1)
-        y = temperature_data['temperature_index']
-        clf = RandomForestClassifier(n_estimators=100)
-        clf.fit(X, y)
-        temperature_index = clf.predict(X)
-        return temperature_index
+    def activate_temperature_control(self):
+        # Activate temperature control system using ROS
+        rospy.init_node('temperature_control_node')
+        self.temperature_control_system = ['temperature_control_system_1', 'temperature_control_system_2', 'temperature_control_system_3']
+        self.temperature = 22
 
-    def activate_heating_system(self):
-        # Activate heating system using robotics and automation system
-        pass
-
-    def deactivate_heating_system(self):
-        # Deactivate heating system using robotics and automation system
-        pass
+    def deactivate_temperature_control(self):
+        # Deactivate temperature control system using ROS
+        rospy.init_node('temperature_control_node')
+        self.temperature_control_system = []
+        self.temperature = 0
 
 class HumidityControl:
     def __init__(self):
-        self.humidity_data = pd.DataFrame(columns=['Humidity'])
-        self.humidification_system = []
+        self.humidity = 0
+        self.humidity_control_system = []
 
-    def get_humidity_data(self):
-        return self.humidity_data
+    def get_humidity(self):
+        return self.humidity
 
-    def calculate_humidity_index(self, humidity_data):
-        # Use machine learning model to calculate humidity index
-        X = humidity_data.drop(['humidity_index'], axis=1)
-        y = humidity_data['humidity_index']
-        clf = RandomForestClassifier(n_estimators=100)
-        clf.fit(X, y)
-        humidity_index = clf.predict(X)
-        return humidity_index
+    def activate_humidity_control(self):
+        # Activate humidity control system using ROS
+        rospy.init_node('humidity_control_node')
+        self.humidity_control_system = ['humidity_control_system_1', 'humidity_control_system_2', 'humidity_control_system_3']
+        self.humidity = 50
 
-    def activate_humidification_system(self):
-        # Activate humidification system using robotics and automation system
-        pass
+    def deactivate_humidity_control(self):
+        # Deactivate humidity control system using ROS
+        rospy.init_node('humidity_control_node')
+        self.humidity_control_system = []
+        self.humidity = 0
 
-    def deactivate_humidification_system(self):
-        # Deactivate humidification system using robotics and automation system
-        pass
+class LightingControl:
+    def __init__(self):
+        self.lighting_level = 0
+        self.lighting_control_system = []
 
-# Example usage:
-habitat = Habitat()
-life_support_system = LifeSupportSystem(habitat)
-air_quality_index = life_support_system.monitor_air_quality()
-life_support_system.control_air_quality(air_quality_index)
+    def get_lighting_level(self):
+        return self.lighting_level
+
+    def activate_lighting_control(self):
+        # Activate lighting control system using ROS
+        rospy.init_node('lighting_control_node')
+        self.lighting_control_system = ['lighting_control_system_1', 'lighting_control_system_2', 'lighting_control_system_3']
+        self.lighting_level = 100
+
+    def deactivate_lighting_control(self):
+        # Deactivate lighting control system using ROS
+        rospy.init_node('lighting_control_node')
+        self.lighting_control_system = []
+        self.lighting_level = 0
+
+class AirQualityControl:
+    def __init__(self):
+        self.air_quality_level = 0
+        self.air_quality_control_system = []
+
+    def get_air_quality_level(self):
+        return self.air_quality_level
+
+    def activate_air_quality_control(self):
+        # Activate air quality control system using ROS
+        rospy.init_node('air_quality_control_node')
+        self.air_quality_control_system = ['air_quality_control_system_1', 'air_quality_control_system_2', 'air_quality_control_system_3']
+        self.air_quality_level = 100
+
+    def deactivate_air_quality_control(self):
+        # Deactivate air quality control system using ROS
+        rospy.init_node('air_quality_control_node')
+        self.air_quality_control_system = []
+        self.air_quality_level = 0
