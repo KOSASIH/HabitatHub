71ea96ca319b1062ff33cbbc209edd75aa59a043
KOSASIH <kosasihg88@gmail.com>
2024-10-14 10:08:48 +0700

    Create systems.py

---
+++ src/systems/systems.py
@@ -0,0 +1,143 @@
+# systems.py
+
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from scipy.optimize import minimize
+from scipy.integrate import odeint
+from matplotlib.pyplot import plot, show, xlabel, ylabel, title
+import rospy
+import roslib
+import tf
+
+class HabitatHub:
+    def __init__(self):
+        self.airlock_system = AirlockSystem()
+        self.communication_system = CommunicationSystem()
+        self.navigation_system = NavigationSystem()
+        self.propulsion_system = PropulsionSystem()
+
+    def monitor_systems(self):
+        airlock_status = self.airlock_system.get_airlock_status()
+        communication_status = self.communication_system.get_communication_status()
+        navigation_status = self.navigation_system.get_navigation_status()
+        propulsion_status = self.propulsion_system.get_propulsion_status()
+        return airlock_status, communication_status, navigation_status, propulsion_status
+
+    def control_systems(self, airlock_status, communication_status, navigation_status, propulsion_status):
+        if airlock_status < self.airlock_system.airlock_requirement:
+            self.airlock_system.activate_airlock()
+        elif airlock_status > self.airlock_system.airlock_requirement:
+            self.airlock_system.deactivate_airlock()
+
+        if communication_status < self.communication_system.communication_requirement:
+            self.communication_system.activate_communication()
+        elif communication_status > self.communication_system.communication_requirement:
+            self.communication_system.deactivate_communication()
+
+        if navigation_status < self.navigation_system.navigation_requirement:
+            self.navigation_system.activate_navigation()
+        elif navigation_status > self.navigation_system.navigation_requirement:
+            self.navigation_system.deactivate_navigation()
+
+        if propulsion_status < self.propulsion_system.propulsion_requirement:
+            self.propulsion_system.activate_propulsion()
+        elif propulsion_status > self.propulsion_system.propulsion_requirement:
+            self.propulsion_system.deactivate_propulsion()
+
+    def optimize_systems(self):
+        # Use machine learning model to optimize systems
+        X = pd.DataFrame({'airlock_status': [self.airlock_system.get_airlock_status()], 
+                           'communication_status': [self.communication_system.get_communication_status()], 
+                           'navigation_status': [self.navigation_system.get_navigation_status()], 
+                           'propulsion_status': [self.propulsion_system.get_propulsion_status()]})
+        y = pd.DataFrame({'system_performance': [self.monitor_systems()]})
+        model = RandomForestRegressor(n_estimators=100)
+        model.fit(X, y)
+        optimized_performance = model.predict(X)
+        return optimized_performance
+
+class AirlockSystem:
+    def __init__(self):
+        self.airlock_status = 0
+        self.airlock_requirement = 100
+        self.airlock_system = []
+
+    def get_airlock_status(self):
+        return self.airlock_status
+
+    def activate_airlock(self):
+        # Activate airlock system using ROS
+        rospy.init_node('airlock_node')
+        self.airlock_system = ['airlock_system_1', 'airlock_system_2', 'airlock_system_3']
+        self.airlock_status = 100
+
+    def deactivate_airlock(self):
+        # Deactivate airlock system using ROS
+        rospy.init_node('airlock_node')
+        self.airlock_system = []
+        self.airlock_status = 0
+
+class CommunicationSystem:
+    def __init__(self):
+        self.communication_status = 0
+        self.communication_requirement = 100
+        self.communication_system = []
+
+    def get_communication_status(self):
+        return self.communication_status
+
+    def activate_communication(self):
+        # Activate communication system using ROS
+        rospy.init_node('communication_node')
+        self.communication_system = ['communication_system_1', 'communication_system_2', 'communication_system_3']
+        self.communication_status = 100
+
+    def deactivate_communication(self):
+        # Deactivate communication system using ROS
+        rospy.init_node('communication_node')
+        self.communication_system = []
+        self.communication_status = 0
+
+class NavigationSystem:
+    def __init__(self):
+        self.navigation_status = 0
+        self.navigation_requirement = 100
+        self.navigation_system = []
+
+    def get_navigation_status(self):
+        return self.navigation_status
+
+    def activate_navigation(self):
+        # Activate navigation system using ROS
+        rospy.init_node('navigation_node')
+        self.navigation_system = ['navigation_system_1', 'navigation_system_2', 'navigation_system_3']
+        self.navigation_status = 100
+
+    def deactivate_navigation(self):
+        # Deactivate navigation system using ROS
+        rospy.init_node('navigation_node')
+        self.navigation_system = []
+        self.navigation_status = 0
+
+class PropulsionSystem:
+    def __init__(self):
+        self.propulsion_status = 0
+        self.propulsion_requirement = 100
+        self.propulsion_system = []
+
+    def get_propulsion_status(self):
+        return self.propulsion_status
+
+    def activate_propulsion(self):
+        # Activate propulsion system using ROS
+        rospy.init_node('propulsion_node')
+        self.propulsion_system = ['propulsion_system_1', 'propulsion_system_2', 'propulsion_system_3']
+        self.propulsion_status = 100
+
+    def deactivate_propulsion(self):
+        # Deactivate propulsion system using ROS
+        rospy.init_node('propulsion_node')
+        self.propulsion_system = []
+        self.propulsion_status = 0
